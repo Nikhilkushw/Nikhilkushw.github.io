470572f49333c7b14e47e4e2633cd0c6520b81ca
Nikhil Repository <nikhilkush037@gmail.com>
2024-05-08 10:21:21 +0530

    Add

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter as Router} from 'react-router-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    {/* <Router> */}
+    <HashRouter>
       <App />
-    </Router>
-  </React.StrictMode>,
-)
+    </HashRouter>
+    {/* </Router> */}
+  </React.StrictMode>
+);
